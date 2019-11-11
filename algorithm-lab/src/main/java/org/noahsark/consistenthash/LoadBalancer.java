dda65e109aadf41553558a65636bcf96ac63a7a7
allen <zhangxt@hst.com>
2019-11-11 15:49:14 +0800

    添加一致性哈希算法

---
+++ algorithm-lab/src/main/java/org/noahsark/consistenthash/LoadBalancer.java
@@ -0,0 +1,12 @@
+package org.noahsark.consistenthash;
+
+import java.util.List;
+
+/**
+ * @author daofeng.xjf
+ * @date 2019/2/16
+ */
+public interface LoadBalancer {
+
+    Server select(List<Server> servers, Invocation invocation);
+}
---
+++ algorithm-lab/src/main/java/org/noahsark/consistenthash/Server.java
@@ -0,0 +1,25 @@
+package org.noahsark.consistenthash;
+
+/**
+ * @author daofeng.xjf
+ * @date 2019/2/15
+ */
+public class Server {
+
+    public Server() {
+    }
+
+    public Server(String url) {
+        this.url = url;
+    }
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
