aaed26aab4c1ae9104f3caf5af73cccc29fd3aab
allen <zhangxt@fsmeeting.com>
2023-02-04 13:25:22 +0800

    commit fastjar lab

---
+++ fastjar-lab/src/main/java/org/noahsark/fastjar/App.java
@@ -0,0 +1,17 @@
+package org.noahsark.fastjar;
+
+/**
+ * 程序主类
+ *
+ * @author zhangxt
+ * @date 2023/02/03 18:30
+ **/
+public class App {
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Current Timestamp:" + System.currentTimeMillis());
+        }
+    }
+}
